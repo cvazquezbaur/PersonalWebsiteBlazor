f6ad12a3a1a0a022060944e69249be3f51328438
Carlos VazquezBaur <Carlos.VazquezBaur@nmhealth.org>
2025-03-25 16:25:19 -0600

    First Commit

---
+++ PersonalWebsite/Components/Models/ContactInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection.Metadata;
+
+namespace PersonalWebsite;
+
+public class ContactInfo
+{
+    public string? Name { get; set;}
+    public string? Email { get; set;}
+    public string? PhoneNumber { get; set;}
+    public string? Message { get; set;}
+}
