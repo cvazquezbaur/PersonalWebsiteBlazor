d6972b785ef2810e30097beb088de0759bc064e3
Carlos VazquezBaur <Carlos.VazquezBaur@nmhealth.org>
2025-03-25 17:05:43 -0600

    editing singing page

--- PersonalWebsite/obj/Debug/net9.0/PersonalWebsite.AssemblyInfo.cs
+++ PersonalWebsite/obj/Debug/net9.0/PersonalWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6ad12a3a1a0a022060944e69249be3f51328438")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
